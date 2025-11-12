08f5d3ab34ecb85ce84134bf0c81f16829ca7664
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-12 16:25:28 +0000

    creating the sign in componet

--- vue-project/src/componets/Signin.vue
+++ vue-project/src/componets/Signin.vue
@@ -3,6 +3,19 @@
 
 <template>
     <div class="max-w-sm m-auto my-8">
-        
+        <div class="border p-10 border-grey-light shadow rounded">
+            <h3 class="text-2xl mb-6 text-grey:900">Sign In</h3>
+            <form @submit.prevent="signin">
+                <div class="text-red" v-if="Error">{{ error }}</div>
+                <div class="mb-6">
+                    <label for="username">Username: </label>
+                    <input type="text" v-model="username" id="username" placeholder="JohnDoeKnowsMoe">
+                    <label for="email" >Email: </label>
+                    <input type="email" v-model="email" id="email" placeholder="JohnDoe@email.com">
+                    <label for="password">Password: </label>
+                    <input type="text" v-model="password" id="password" placeholder="SuperSecretPassword123">
+                </div>
+            </form>
+        </div>
     </div>
 </template>
