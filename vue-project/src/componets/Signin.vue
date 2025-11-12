d2f00292505660df13babf0c1e0d41acbfaa1440
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-12 18:37:15 +0000

    Still setting up the logic for the sign in

--- vue-project/src/componets/Signin.vue
+++ vue-project/src/componets/Signin.vue
@@ -22,8 +22,20 @@ export default {
             .then(response => this.signinSuccessful(response))
             .catch(error => this.signinFailed(error))
         },
-        signinSuccessful() {
-            this.$http.secured.post('/signin')
+        signinSuccessful(response) {
+            if(!response.data.csrf){
+                this.signinFailed(response)
+                return
+            } 
+            localStorage.csrf = response.data.csrf
+            localStorage.signedIn = true
+            this.error = ''
+            this.$router.replace('/songs')
+        },
+        signinFailed(error){
+            this.error = (error.response && error.response.data && error.response.data.error) || ''
+            delete localStorage.csrf
+            delete localStorage.signedIn
         }
     }
 }
