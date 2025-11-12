71335f3e541b8b3b2cf7fc961240500bc5c5373f
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-12 18:29:35 +0000

    Setting up the vue side logic for the sign in

--- vue-project/src/componets/Signin.vue
+++ vue-project/src/componets/Signin.vue
@@ -1,4 +1,32 @@
 <script>
+
+export default {
+    name:'Signin',
+    data () {
+        return {
+            username: '',
+            email: '',
+            password: '',
+            error: ''
+        }
+    },
+    created() {
+        this.checkSignIn()
+    },
+    updated() {
+        this.checkSignIn()
+    },
+    methods: {
+        signin() {
+            this.$http.plain.post('/signin', { username: this.username, email: this.email, password: this.password} )
+            .then(response => this.signinSuccessful(response))
+            .catch(error => this.signinFailed(error))
+        },
+        signinSuccessful() {
+            this.$http.secured.post('/signin')
+        }
+    }
+}
 </script>
 
 <template>
@@ -10,11 +38,24 @@
                 <div class="mb-6">
                     <label for="username">Username: </label>
                     <input type="text" v-model="username" id="username" placeholder="JohnDoeKnowsMoe">
+                </div>
+
+                <div class="mb-6">
                     <label for="email" >Email: </label>
                     <input type="email" v-model="email" id="email" placeholder="JohnDoe@email.com">
+                </div>
+
+                <div class="mb-6">
                     <label for="password">Password: </label>
                     <input type="text" v-model="password" id="password" placeholder="SuperSecretPassword123">
-                </div>
+               </div>
+               <button type="submit" class="font-bold px-4 rounded cursor-pointer no-underline bg-gradient-to-b from-[#0072FF] to-[#00C853] block w-full py-4 text-white items-center justify-center">
+                Sign In
+               </button>
+
+               <div class="my-4">
+                <router-link to="/signup" class="">Sign Up</router-link>
+               </div>
             </form>
         </div>
     </div>
