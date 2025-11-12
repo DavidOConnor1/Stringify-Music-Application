f6587e0a77e5033714f3b8f79c5bd6f35e8242ac
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-12 14:59:00 +0000

    Setting up axios to call from the Ruby API/Backend

---
+++ vue-project/src/backend/axios/index.js
@@ -0,0 +1,47 @@
+import { config } from "@vue/test-utils";
+import axios from "axios";
+
+const API_URL = 'http://localhost:3000'
+
+const securedAxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+    headers: {
+        'getContentType': 'application/json'
+    }
+})
+
+const plainAxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+securedAxiosInstance.interceptors.request.use(config => {
+    const method = config.method.toUpperCase();
+    if(method !== 'OPTIONS' && method !== 'GET'){
+        config.headers = {
+            ...config.headers, //separates into an array
+            'X-CSRF-TOKEN': localStorage.crsf
+        }
+    }
+
+    return config
+});
+
+securedAxiosInstance.interceptors.response.use(null, error => {
+    if(error.response && error.response.config && error.response.status === 401){
+        //post to refresh
+        return plainAxiosInstance.post('/refresh', {}, { headers: {'X-CSRF-TOKEN': localStorage.crsf}})
+        .then(response => {
+            localStorage.csrf = response.data.csrf
+            localStorage.signedIn = true
+
+            let retryConfig = error.response.config
+            retryConfig.headers['CSRF-TOKEN'] = localStorage.csrf
+            return plainAxiosInstance.request(retryConfig)
+        }).catch(error => )
+    }
+})
