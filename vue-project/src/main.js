dcee6458ff3e6ae95cd90c03e5339c062b009d1c
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-12 14:31:22 +0000

    updating the libraries

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
+import router from './router'
+import VueAxios from 'vue-axios'
 
 
 createApp(App).mount('#app')
