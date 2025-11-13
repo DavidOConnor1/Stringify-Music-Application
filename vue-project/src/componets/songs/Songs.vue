1b0dc377876d670c87016c79e02837c65fec2fa8
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-13 19:56:15 +0000

    finished writing the frontend code for the song controller

--- vue-project/src/componets/songs/Songs.vue
+++ vue-project/src/componets/songs/Songs.vue
@@ -1,61 +1,111 @@
 <script>
-    export default{
-        name: 'Songs',
-        data() {
-            return {
-                artists: [],
-                songs: [],
-                newSong: [],
-                error: '',
-                editedSong: ''
-            }
-        },
-        created() {
-            if(!localStorage.signedIn){
-                this.$router.replace('/')
-            } else {
-                //connecting to the songs controller
-                this.$http.secured.get('/api/v1/songs')
-                .then(response => {this.songs = response.data})
-                .catch(error => this.setError(error, 'An Error has occured when trying to connect to the backend'))
+export default {
+  name: "Songs",
+  data() {
+    return {
+      artists: [],
+      songs: [],
+      newSong: [],
+      error: "",
+      editedSong: "",
+    };
+  },
+  created() {
+    if (!localStorage.signedIn) {
+      this.$router.replace("/");
+    } else {
+      //connecting to the songs controller
+      this.$http.secured
+        .get("/api/v1/songs")
+        .then((response) => {
+          this.songs = response.data;
+        })
+        .catch((error) =>
+          this.setError(
+            error,
+            "An Error has occured when trying to connect to the backend"
+          )
+        );
 
-                //connecting to the artists controller
-                 this.$http.secured.get('/api/v1/artists')
-                .then(response => {this.artists = response.data})
-                .catch(error => this.setError(error, 'An Error has occured when trying to connect to the backend'))
-            }
-        },
-         methods: {
+      //connecting to the artists controller
+      this.$http.secured
+        .get("/api/v1/artists")
+        .then((response) => {
+          this.artists = response.data;
+        })
+        .catch((error) =>
+          this.setError(
+            error,
+            "An Error has occured when trying to connect to the backend"
+          )
+        );
+    }
+  },
+  methods: {
     setError(error, text) {
       this.error =
         (error.response && error.response.data && error.response.data.error) ||
         text;
     },
     getArtist(song) {
-        const songArtistValues = this.artists.filter(artist => artist.id === song.artist_id)
-        
-        //loops through the artists
-        let artist
-        songArtistValues.forEach(function(element) {
-            artist = element.name
-        })
-        return artist;
+      const songArtistValues = this.artists.filter(
+        (artist) => artist.id === song.artist_id
+      );
+
+      //loops through the artists
+      let artist;
+      songArtistValues.forEach(function (element) {
+        artist = element.name;
+      });
+      return artist;
     },
     addSong() {
-        const value = this.newSong
-        if(!value) {
-            return 
-        } else {
-            this.$http.post('/api/v1/songs', { song: {title: this.newSong.title, year: this.newSong.year, artist_id: this.newSong.artist}})
-            .then(response => {
-                this.songs.push(response.data)
-                this.newSong = ''
-            })
-            .catch(this.error, 'There was an error when trying to push the new song to the backend ')
-        }
-    }
-    }
-
+      const value = this.newSong;
+      if (!value) {
+        return;
+      } else {
+        this.$http.secured
+          .post("/api/v1/songs", {
+            song: {
+              title: this.newSong.title,
+              year: this.newSong.year,
+              artist_id: this.newSong.artist,
+            },
+          })
+          .then((response) => {
+            this.songs.push(response.data);
+            this.newSong = "";
+          })
+          .catch((error) =>
+            this.setError(error, "Unable to create a new Song")
+          );
+      }
+    },
+    removeSong(song) {
+      this.$http.secured
+        .delete(`/api/v1/songs/${song.id}`)
+        .then((response) => {
+          this.songs.splice(this.songs.indexOf(song), 1);
+        })
+        .catch((error) =>
+          this.setError(error, "Unable to delete the song at this time")
+        );
+    },
+    editSong(song) {
+      this.editedSong = song;
+    },
+    updateSong(song) {
+      this.editSong = "";
+      this.$http.secured
+        .patch(`/api/v1/songs/${song.id}`, {
+          song: { title: song.title, year: song.year, artist_id: song.artist },
+        })
+        .catch((error) =>
+          this.setError(error, "Unable to update song at this time")
+        );
+    },
+  },
+};
 </script>
 
 <template>
