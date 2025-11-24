27d51e965075fe288f734f266aa98da22bf723db
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-24 15:35:47 +0000

    writing the server logic

--- src/main.js
+++ src/main.js
@@ -13,3 +13,20 @@ app.config.globalProperties.$plainAxios = plainAxiosInstance;
 app.use(router);
 
 app.mount("#app");
+
+const validateTokenOnAppStart = () => {
+    const isSignedIn = localStorage.isSignedIn;
+    const csrfToken = localStorage.csrf;
+
+    if(isSignedIn && csrfToken){
+        securedAxiosInstance.get('/api/v1/songs')
+        .catch(error => {
+            if(error.response && error.response.status === 401) {
+                //if the token is invalid, delete authentication data
+                delete localStorage.csrf;
+                delete localStorage.isSignedIn;
+                console.log('Token invalidated on app start')
+            }
+        })
+    }
+}
